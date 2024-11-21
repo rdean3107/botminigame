afd2f25aed1eed42952799c8d3c7224556c2f4c8
rdean3107 <162683228+rdean3107@users.noreply.github.com>
2024-11-21 07:21:53 +0700

    Create voice.js

---
+++ commands/voice.js
@@ -0,0 +1,38 @@
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
+
+module.exports = {
+    name: 'voice',
+    description: 'Lệnh để bot tham gia và rời khỏi voice channel.',
+
+    async execute(message) {
+        // Lệnh "!join" để bot tham gia voice channel
+        if (message.content === '!join') {
+            if (message.member.voice.channel) {
+                joinVoiceChannel({
+                    channelId: message.member.voice.channel.id,
+                    guildId: message.guild.id,
+                    adapterCreator: message.guild.voiceAdapterCreator,
+                });
+                message.reply('Đã tham gia voice channel!');
+            } else {
+                message.reply('Bạn cần vào một voice channel trước!');
+            }
+        }
+
+        // Lệnh "!leave" để bot rời khỏi voice channel
+        else if (message.content === '!leave') {
+            const voiceChannel = message.member.voice.channel;
+            if (voiceChannel) {
+                const connection = getVoiceConnection(message.guild.id);
+                if (connection) {
+                    connection.destroy();
+                    message.reply('Đã rời khỏi voice channel!');
+                } else {
+                    message.reply('Bot không ở trong voice channel nào.');
+                }
+            } else {
+                message.reply('Bạn cần ở trong một voice channel để sử dụng lệnh này.');
+            }
+        }
+    },
+};
