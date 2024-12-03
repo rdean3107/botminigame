3a8f69040f4e7acd567df8647fa155aacdcb003a
rdean3107 <162683228+rdean3107@users.noreply.github.com>
2024-12-03 18:37:58 +0700

    Update taiXiu.js

--- commands/taiXiu.js
+++ commands/taiXiu.js
@@ -1,25 +1,37 @@
 const { rollDice } = require('../utils');
 const { diceEmojis } = require('../emojis');
 
+const allowedRoles = ['Lắc Tài Xỉu', 'set host'];
+const rollingTime = 2000;
+const emojis = {
+    rolling: '<a:dicegame:1313437370040848445>',
+    finished: '<a:lua:1313461231620853863>',
+    warning: '<a:canhbao:1302598129300013116>',
+    rollingAnimation: '<a:lacdu:1313460032142049290>',
+    separator: '<:kh_cham:1247581986181222531>'
+};
+const logChannelId = '1313465666841612339'; // Thay bằng ID của kênh bạn muốn gửi log
+
 const hasRoleToRoll = (member) => {
-    const allowedRoles = ['Lắc Tài Xỉu', 'set host'];
     return member.roles.cache.some(role => allowedRoles.includes(role.name));
 };
 
 module.exports = async (message, isDitMe, isDuma) => {
     if (!hasRoleToRoll(message.member)) {
-        return message.channel.send("<a:canhbao:1302598129300013116> Bạn cần quyền `Lắc Tài Xỉu` hoặc `set host`!");
+        return message.channel.send(`${emojis.warning} Bạn cần quyền \`Lắc Tài Xỉu\` hoặc \`set host\`!`);
     }
 
     try {
-        const initialMessage = await message.channel.send('<a:lac:1305457531518455870> <a:lac:1305457531518455870> <a:lac:1305457531518455870>');
-        const rollingMessage = await message.channel.send('**<a:danglac:1305461120764743731> Đang lắc...**');
+        const initialMessage = await message.channel.send(`${emojis.rolling} ${emojis.rolling} ${emojis.rolling}`);
+        const rollingMessage = await message.channel.send(`**${emojis.rollingAnimation} Đang lắc...**`);
         const finalResults = [null, null, null];
         let rollCount = 0;
 
         const rollingInterval = setInterval(async () => {
             finalResults[rollCount] = rollDice(isDitMe, isDuma);
-            const diceEmojisString = finalResults.map(result => result ? diceEmojis[result] : '<a:lac:1305457531518455870>').join(' ');
+            const diceEmojisString = finalResults
+                .map(result => result ? diceEmojis[result] : emojis.rolling)
+                .join(' ');
             await initialMessage.edit(diceEmojisString);
             rollCount++;
 
@@ -28,12 +40,23 @@ module.exports = async (message, isDitMe, isDuma) => {
                 const total = finalResults.reduce((acc, num) => acc + num, 0);
                 const resultType = total <= 10 ? 'Xỉu' : 'Tài';
                 const parity = total % 2 === 0 ? 'Chẵn' : 'Lẻ';
-                await rollingMessage.edit(`**<a:money:1305461123121811539> ${total}・${resultType}・${parity} <a:money:1305461123121811539>**`);
-                
-                // Thêm log cho lệnh
-                console.log(`Lệnh bởi ${message.author.tag}: Kết quả Tài Xỉu: ${finalResults.join(', ')} ${resultType} ${parity}`);
+                const resultMessage = `**${emojis.finished} ${total}${emojis.separator}${resultType}${emojis.separator}${parity} ${emojis.finished}**`;
+
+                await rollingMessage.edit(resultMessage);
+
+                // Thêm thời gian vào log
+                const timestamp = new Date().toLocaleString('vi-VN', { hour12: false }); // Thời gian theo định dạng 24 giờ
+                const logMessage = `[${timestamp}] Lệnh bởi __${message.author.tag}__: Kết quả Tài Xỉu: ${finalResults.join(', ')} ${resultType} ${parity}`;
+
+                // Gửi log vào kênh riêng với thời gian
+                const logChannel = message.guild.channels.cache.get(logChannelId);
+                if (logChannel) {
+                    logChannel.send(logMessage);
+                }
+
+                console.log(logMessage);
             }
-        }, 2000);
+        }, rollingTime);
     } catch (error) {
         console.error('Lỗi khi xử lý Tài Xỉu:', error);
         await message.channel.send('Đã xảy ra lỗi khi lắc tài xỉu!');
