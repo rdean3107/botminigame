b55cb9b09e7c70b2eb5596d6083d3e0bef619db8
rdean3107 <162683228+rdean3107@users.noreply.github.com>
2024-12-03 18:37:44 +0700

    Update bauCua.js

--- commands/bauCua.js
+++ commands/bauCua.js
@@ -1,37 +1,71 @@
 const { rollBauCua } = require('../utils');
 const { bauCuaEmojis } = require('../emojis');
 
+const allowedRoles = ['Lắc Bầu Cua', 'set host'];
+const rollingTime = 2000;
+const emojis = {
+    rolling: '<a:lacbaucua:1313456234388525096>',
+    finished: '<a:lua:1313461231620853863>',
+    warning: '<a:canhbao:1302598129300013116>',
+    rollingAnimation: '<a:lacdu:1313460032142049290>',
+    separator: '<:kh_cham:1247581986181222531>'
+};
+const logChannelId = '1313465666841612339'; // Thay bằng ID của kênh bạn muốn gửi log
+
 const hasRoleToRoll = (member) => {
-    const allowedRoles = ['Lắc Bầu Cua', 'set host'];
     return member.roles.cache.some(role => allowedRoles.includes(role.name));
 };
 
 module.exports = async (message, isDitMe) => {
     if (!hasRoleToRoll(message.member)) {
-        return message.channel.send("<a:canhbao:1302598129300013116> Bạn cần quyền `Lắc Bầu Cua` hoặc `set host`!");
+        return message.channel.send(`${emojis.warning} Bạn cần quyền \`Lắc Bầu Cua\` hoặc \`set host\`!`);
     }
 
     try {
-        const initialMessage = await message.channel.send('<a:lac:1302884217503612939> <a:lac:1302884217503612939> <a:lac:1302884217503612939>');
-        const rollingMessage = await message.channel.send('**<a:danglac:1302588793202671616> Đang lắc Bầu Cua...**');
+        const initialMessage = await message.channel.send(`${emojis.rolling} ${emojis.rolling} ${emojis.rolling}`);
+        const rollingMessage = await message.channel.send(`**${emojis.rollingAnimation} Đang lắc...**`);
         const finalResults = [null, null, null];
         let rollCount = 0;
 
         const rollingInterval = setInterval(async () => {
             finalResults[rollCount] = rollBauCua(isDitMe);
-            const bauCuaEmojisString = finalResults.map(result => result ? bauCuaEmojis[result] : '<a:lac:1302884217503612939>').join(' ');
+            const bauCuaEmojisString = finalResults
+                .map(result => result ? bauCuaEmojis[result] : emojis.rolling)
+                .join(' ');
             await initialMessage.edit(bauCuaEmojisString);
             rollCount++;
 
             if (rollCount >= 3) {
                 clearInterval(rollingInterval);
-                const resultsString = finalResults.join('<a:cham:1302892048671707207>'); // Kết quả cuối cùng
-                await rollingMessage.edit(`**<a:money:1302576106435645491> ${resultsString} <a:money:1302576106435645491>**`);
+                const resultsString = finalResults.join(emojis.separator);
+                await rollingMessage.edit(`**${emojis.finished} ${resultsString} ${emojis.finished}**`);
+
+                // Lấy thời gian hiện tại theo định dạng [hh:mm:ss dd/MM/yyyy]
+                const timestamp = new Date().toLocaleString('vi-VN', {
+                    hour12: false, 
+                    timeZone: 'Asia/Ho_Chi_Minh'
+                }).replace(/(\d{2}:\d{2}:\d{2}) (\d{2})\/(\d{2})\/(\d{4})/, '[$1 $3/$2/$4]');
                 
-                // Thêm log cho lệnh
-                console.log(`Lệnh bởi ${message.author.tag}: Kết quả Bầu Cua: ${finalResults.join(', ')}`);
+                // Chuyển tên emoji thành tên chữ tương ứng (ví dụ: "cua", "tom", ...)
+                const resultNames = finalResults.map(result => {
+                    if (result !== null) {
+                        return bauCuaEmojis[result].name || result;  // Lấy tên nếu có
+                    }
+                    return result;
+                });
+
+                // Tạo log message theo định dạng yêu cầu
+                const logMessage = `[${timestamp}] Lệnh bởi __${message.author.tag}__: Kết quả Bầu Cua: ${resultNames.join(', ')}`;
+
+                // Gửi log vào kênh riêng với thời gian
+                const logChannel = message.guild.channels.cache.get(logChannelId);
+                if (logChannel) {
+                    logChannel.send(logMessage);
+                }
+
+                console.log(logMessage);
             }
-        }, 2000);
+        }, rollingTime);
     } catch (error) {
         console.error('Lỗi khi xử lý Bầu Cua:', error);
         await message.channel.send('Đã xảy ra lỗi khi lắc bầu cua!');
