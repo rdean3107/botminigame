8b65b916854ab6165fb8538a9eee80fa98abd463
rdean3107 <162683228+rdean3107@users.noreply.github.com>
2024-12-12 21:01:47 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,14 +1,16 @@
 module.exports = async (message) => {
     const msg = await message.channel.send('🏓 Đang kiểm tra độ trễ...');
-    const ping = msg.createdTimestamp - message.createdTimestamp; // Tính độ trễ
+    const messagePing = msg.createdTimestamp - message.createdTimestamp; // Độ trễ tin nhắn
+    const apiPing = Math.round(message.client.ws.ping); // Độ trễ API
 
     // Tính thời gian hoạt động của bot
     const uptime = process.uptime();
-    const hours = Math.floor(uptime / 3600);
+    const days = Math.floor(uptime / 86400);
+    const hours = Math.floor((uptime % 86400) / 3600);
     const minutes = Math.floor((uptime % 3600) / 60);
     const seconds = Math.floor(uptime % 60);
 
-    const uptimeString = `${hours} giờ, ${minutes} phút, ${seconds} giây`;
+    const uptimeString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
-    await msg.edit(`🏓 Độ trễ: ${ping}ms\n⏱️ Bot đã hoạt động: ${uptimeString}`);
+    await msg.edit(`🏓 Pong!\nĐộ trễ tin nhắn\n${messagePing}ms\nĐộ trễ API\n${apiPing}ms\nThời gian hoạt động\n${uptimeString}\n\nYêu cầu bởi ${message.author}`);
 };
